0a22660a9c046cae891641f4800905407c50b0cf
K <1339764596@qq.com>
2020-01-07 21:34:11 +0800

    push

--- run_test.sh
+++ run_test.sh
@@ -3,11 +3,10 @@ BLOCK_SIZE=$((4*4096))
 TOTAL_SIZE=$((16*1024))
 
 mkdir $RESULT_PATH
-dd if=/dev/zero of=0.io bs=$TOTAL_SIZE count=$((1024*1024))
 for ((i=1; i<=3; i++))
 do
 rm -rf 0.io
-for ((j=1; j<=5; j++))
+for ((j=1; j<=3; j++))
 do
 perf record ./tester $i 1 $BLOCK_SIZE $TOTAL_SIZE
 mv perf.data $RESULT_PATH/$i_$j
