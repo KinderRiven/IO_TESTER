56d46a34c334b5f6a7324b27d8825d060928ac86
K <1339764596@qq.com>
2020-01-09 14:21:05 +0800

    push

--- run_dd.sh
+++ run_dd.sh
@@ -9,9 +9,9 @@ rm -rf 0.io
 dd if=/dev/zero of=0.io bs=$TOTAL_SIZE count=$((1024*1024))
 for ((j=1; j<=10; j++))
 do
-echo "running {$i}_{$j}..."
+echo "running $i_$j..."
 perf record ./tester $i 1 $BLOCK_SIZE $TOTAL_SIZE
-mv perf.data $RESULT_PATH/{$i}_{$j}
+mv perf.data $RESULT_PATH/$i_$j
 sync && echo 3 > /proc/sys/vm/drop_caches
 done
 done
