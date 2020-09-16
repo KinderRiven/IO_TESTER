49907ced561ea864145fa262b4f0501741dad5b0
K <1339764596@qq.com>
2020-09-16 18:08:26 +0800

    ffff

--- spdk_example/thread_example.cc
+++ spdk_example/thread_example.cc
@@ -27,7 +27,7 @@ void start_app(void* cb)
     struct app_msg_t* msg = (struct app_msg_t*)cb;
     struct spdk_bdev* bdev;
 
-    printf(">>>>start_thread (0x%llx)\n", (uint64_t)cb);
+    printf(">>>>[start_thread(0x%llx)]\n", (uint64_t)cb);
 
     bdev = spdk_bdev_get_by_name(msg->bdev_name);
     printf("    [bdev:%s|%llx]\n", msg->bdev_name, (uint64_t)bdev);
@@ -35,14 +35,14 @@ void start_app(void* cb)
 
 int bdev_parse_arg(int ch, char* arg)
 {
-    printf(">>>>bdev_parse_arg (%c) (%s)\n", ch, arg);
+    printf(">>>>[bdev_parse_arg(%c)-(%s)]\n", ch, arg);
     g_bdev_name = arg;
     return 0;
 }
 
 void bdev_usage()
 {
-    printf(">>>>bdev_usage\n");
+    printf(">>>>[bdev_usage]\n");
 }
 
 int main(int argc, char** argv)
@@ -55,15 +55,15 @@ int main(int argc, char** argv)
     opts.name = "bdev-example";
 
     if ((rc = spdk_app_parse_args(argc, argv, &opts, "b:", NULL, bdev_parse_arg, bdev_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
-        printf(">>>>spdk_app_parse_arg error!\n");
+        printf(">>>>[spdk_app_parse_arg error!]\n");
         exit(rc);
     }
-    app_msg.bdev_name = g_bdev_name;
 
+    app_msg.bdev_name = g_bdev_name;
     printf("OPT [name:%s][file_name:%s]\n", opts.name, opts.config_file);
     printf("APP [name:%s]\n", app_msg.bdev_name);
-
     rc = spdk_app_start(&opts, start_app, nullptr);
+
     spdk_app_stop(rc);
     return 0;
 }
