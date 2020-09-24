4007e6210848228b25e35e29969a02a4229532de
K <1339764596@qq.com>
2020-09-24 14:03:52 +0800

    ffff

--- pmdk_example/pmdk_pmem_test.cc
+++ pmdk_example/pmdk_pmem_test.cc
@@ -11,11 +11,33 @@
 
 #include "pmdk/libpmem.h"
 
+void do_write(void* base, size_t space_size, size_t data_size, size_t block_size)
+{
+    char* buff = new char[block_size];
+    size_t one_turn = space_size / block_size;
+    size_t turn_count = data_size / space_size;
+
+    printf("[turn_count:%d][one_turn:%d]\n", turn_count, one_turn);
+
+    for (size_t i = 0; i < turn_count; i++) {
+        char* addr = (char*)base;
+        for (size_t j = 0; j < one_turn; j++) {
+            pmem_memcpy_persist((void*)(addr), buff, block_size);
+            addr += block_size;
+        }
+    }
+}
+
+void do_read(void* base, size_t space_size, size_t data_size, size_t block_size)
+{
+}
+
 int main(int argc, char** argv)
 {
     size_t mmap_len;
     int is_pmem;
     void* addr = pmem_map_file("/home/pmem0/test", (size_t)1024 * 1024 * 1024, PMEM_FILE_CREATE, 0777, &mmap_len, &is_pmem);
     printf("[addr:0x%x][mmap_len:%zu][is_pmem:%d]\n", (uint64_t)addr, mmap_len, is_pmem);
+    do_write(addr, mmap_len, mmap_len * 5, 256);
     return 0;
 }
