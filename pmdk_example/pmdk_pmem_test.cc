2a091573e85eecce19653266b300789f1940293d
K <1339764596@qq.com>
2020-09-24 13:48:51 +0800

    ffff

--- pmdk_example/pmdk_pmem_test.cc
+++ pmdk_example/pmdk_pmem_test.cc
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 {
     size_t mmap_len;
     int is_pmem;
-    void* addr = pmem_map_file("/home/pmem0/test", (size_t)1024 * 1024 * 1024, 0, PMEM_FILE_CREATE, &mmap_len, &is_pmem);
+    void* addr = pmem_map_file("/home/pmem0/test", (size_t)1024 * 1024 * 1024, PMEM_FILE_CREATE, 0777, &mmap_len, &is_pmem);
     printf("[addr:0x%x][mmap_len:%zu][is_pmem:%d]\n", (uint64_t)addr, mmap_len, is_pmem);
     return 0;
 }
