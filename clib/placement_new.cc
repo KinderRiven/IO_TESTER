83f01d1564cd96dcab201cb502c4d68b9595d3d3
K <1339764596@qq.com>
2020-09-28 19:38:48 +0800

    commit by Shukai Han

--- clib/placement_new.cc
+++ clib/placement_new.cc
@@ -2,14 +2,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 int main()
 {
     char* pool = new char[1024 * 1024];
     printf("%llu\n", (uint64_t)pool);
-
     for (int i = 0; i < 10; i++) {
-        char* p = new (pool) char[1024];
+        std::string* p = new (pool) std::string("hello");
         printf("%llu\n", (uint64_t)p);
     }
     return 0;
