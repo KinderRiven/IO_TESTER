1ff60c3c0a1b81c2dbe9efbf4ed4a7ca708431f9
K <1339764596@qq.com>
2020-10-19 22:24:29 +0800

    commit by Shukai Han

--- pmdk_example/pmdk_log_test.cc
+++ pmdk_example/pmdk_log_test.cc
@@ -19,8 +19,10 @@ int main(int argc, char** argv)
     size_t _mmap_len;
     // void* addr = pmem_map_file(_path, (size_t)2 * 1024 * 1024 * 1024, PMEM_FILE_CREATE, 0666, &_mmap_len, &_is_pmem);
     PMEMlogpool* _pool = pmemlog_create(_path, (size_t)2 * 1024 * 1024 * 1024, 0666);
+    if (_pool == nullptr) {
+        _pool = pmemlog_open(_path);
+    }
     assert(_pool != nullptr);
-
     size_t _nb = pmemlog_nbyte(_pool);
     printf("%zuMB\n", _nb / (1024 * 1024));
     return 0;
