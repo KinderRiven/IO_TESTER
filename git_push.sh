7cc7e442ff49536c1043536b02da7c6ff7b4fe67
K <1339764596@qq.com>
2020-10-24 20:46:14 +0800

    commit by Shukai Han

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,4 @@
-git add *.h *.sh
+git add *.h *.sh *.cc
 git add io_tester
 git add example
 git add clib
@@ -7,7 +7,6 @@ git add spdk_example
 git add pmdk_example
 git add Makefile
 git add fio
-git detail.cc
 git add third-party/*.zip third-party/README.md
 git commit -m "commit by Shukai Han"
 git push
