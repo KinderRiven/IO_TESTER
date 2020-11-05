e875976d3b195c0240a889f60913e526f0480ce1
K <1339764596@qq.com>
2020-11-05 14:06:38 +0800

    commit by Shukai Han

--- tail_latency/spdk.cc
+++ tail_latency/spdk.cc
@@ -117,7 +117,7 @@ int main(int argc, char** argv)
     int _res;
     spdk_device_t _dev;
     _dev.transport_string = "trtype:PCIe traddr:0000:19:00.0";
-    _res = spdk_nvme_transport_id_parse(_dev.trid, _dev.transport_string.c_str());
+    _res = spdk_nvme_transport_id_parse(&_dev.trid, _dev.transport_string.c_str());
 
     init_spdk_device(&_dev);
     printf("[%s-%zuGB]\n", _dev.transport_string.c_str(), _dev.capacity / (1024 * 1024 * 1024));
