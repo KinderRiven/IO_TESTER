333b2ea4834d5f1dc13f4af998006785c2216457
K <1339764596@qq.com>
2021-01-18 16:39:18 +0800

    commit by Shukai Han

--- pmdk_example/pmdk_object_test.cc
+++ pmdk_example/pmdk_object_test.cc
@@ -18,14 +18,21 @@ int main(int argc, char** argv)
     int _is_pmem;
     size_t _mmap_len;
     size_t _pool_size = 2UL * 1024 * 1024 * 1024;
-    PMEMobjpool* _pool = nullptr;
+    PMEMobjpool* _index = nullptr;
+    PMEMobjpool* _data = nullptr;
     const char _path[] = "/home/pmem0/pool";
 
     // void* addr = pmem_map_file(_path, (size_t)2 * 1024 * 1024 * 1024, PMEM_FILE_CREATE, 0666, &_mmap_len, &_is_pmem);
-    _pool = pmemobj_create(_path, "btree", _pool_size, 0666);
+    _index = pmemobj_create(_path, "index", _pool_size, 0666);
+    if (_index == nullptr) {
+        printf("existed index!\n");
+        _index = pmemobj_open(_path, "data");
+    }
 
-    if (_pool == nullptr) {
-        printf("create error!\n");
+    _data = pmemobj_create(_path, "data", _pool_size, 0666);
+    if (_data == nullptr) {
+        printf("existed data!\n");
+        _data = pmemobj_open(_path, "data");
     }
     return 0;
 }
