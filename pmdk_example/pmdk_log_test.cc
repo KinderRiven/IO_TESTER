971a10dd14247ac432c7019d4c082ea7aaa1f100
K <1339764596@qq.com>
2021-01-18 16:01:34 +0800

    commit by Shukai Han

--- pmdk_example/pmdk_log_test.cc
+++ pmdk_example/pmdk_log_test.cc
@@ -16,10 +16,11 @@ int main(int argc, char** argv)
 {
     int _is_pmem;
     size_t _mmap_len;
+    PMEMlogpool* _pool;
     char _path[] = "/home/pmem0/pool";
 
     // void* addr = pmem_map_file(_path, (size_t)2 * 1024 * 1024 * 1024, PMEM_FILE_CREATE, 0666, &_mmap_len, &_is_pmem);
-    PMEMlogpool* _pool = pmemlog_create(_path, (size_t)2 * 1024 * 1024 * 1024, 0666);
+    _pool = pmemlog_create(_path, (size_t)2 * 1024 * 1024 * 1024, 0666);
 
     if (_pool == nullptr) {
         _pool = pmemlog_open(_path);
